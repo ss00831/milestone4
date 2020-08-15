9a83a5ccefd00e7e7707791ef2291b1b0be132b9
ss00831 <songhei_831.lee@hotmail.com>
2020-08-15 12:35:43 +0000

    Added search function

--- tourprograms/views.py
+++ tourprograms/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Tourprogram
 
 # Create your views here.
@@ -7,16 +9,28 @@ def all_tourprograms(request):
     """ A view to show all tour programs, including sorting and search queries """
     
     tourprograms = Tourprogram.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('tourprograms'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            tourprograms = tourprograms.filter(queries)
 
     context = {
         'tourprograms': tourprograms,
+        'search_term': query,
     }
 
     return render(request, 'tourprograms/tourprograms.html', context)
 
 
 def tourprogram_detail(request, tourprogram_id):
-    """ A view to show individual product details """
+    """ A view to show individual tourprogram details """
 
     tourprogram = get_object_or_404(Tourprogram, pk=tourprogram_id)
 
