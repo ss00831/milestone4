c134dc365ee8656baa2db749ccc78b006de5eb62
ss00831 <songhei_831.lee@hotmail.com>
2020-08-15 13:12:23 +0000

    Added sort function

--- tourprograms/views.py
+++ tourprograms/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Tourprogram
+from .models import Tourprogram, Category
 
 # Create your views here.
 
@@ -10,8 +10,29 @@ def all_tourprograms(request):
     
     tourprograms = Tourprogram.objects.all()
     query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                tourprograms = tourprograms.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            tourprograms = tourprograms.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            tourprograms = tourprograms.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,9 +42,13 @@ def all_tourprograms(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             tourprograms = tourprograms.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'tourprograms': tourprograms,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'tourprograms/tourprograms.html', context)
