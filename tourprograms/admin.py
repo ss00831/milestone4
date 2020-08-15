4c01b2624ac328688ad6848b5991a11a77f84bf6
ss00831 <songhei_831.lee@hotmail.com>
2020-08-15 11:42:55 +0000

    Customized admin mode

--- tourprograms/admin.py
+++ tourprograms/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Tourprogram, Category
 
 # Register your models here.
-admin.site.register(Tourprogram)
-admin.site.register(Category)
+
+class TourprogramAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'name',
+        'region',
+        'maximum',
+        'price_adult',
+        'price_child',
+        'departure_date',
+        'estimated_times',
+        'rating',
+        'meeting_place',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Tourprogram, TourprogramAdmin)
+admin.site.register(Category, CategoryAdmin)
