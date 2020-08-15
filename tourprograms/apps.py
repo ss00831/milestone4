c3f13bdf36a0d72a286377e04c8997bfa8435299
ss00831 <songhei_831.lee@hotmail.com>
2020-08-15 10:35:49 +0000

    created tourprograms app and add fixtures

---
+++ tourprograms/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TourprogramsConfig(AppConfig):
+    name = 'tourprograms'
---
+++ tourprograms/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Tourprogram(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    region = models.CharField(max_length=254)
+    maximum = models.CharField(max_length=254)
+    price_adult = models.IntegerField()
+    price_child = models.IntegerField()
+    departure_date = models.CharField(max_length=254)
+    estimated_times = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    included = models.CharField(max_length=254, null=True, blank=True)
+    not_included = models.CharField(max_length=254, null=True, blank=True)
+    meeting_place = models.CharField(max_length=254)
+    note = models.TextField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
