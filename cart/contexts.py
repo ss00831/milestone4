b5e93d263d9a370c232bb057a2cf86130f1fcd71
ss00831 <songhei_831.lee@hotmail.com>
2020-08-17 08:22:28 +0000

    updated context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,21 +1,43 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from tourprograms.models import Tourprogram
+
 
 def cart_contents(request):
 
     cart_items = []
-    total = 0
+    total_adult = 0
+    #total_child = 0
     tourprogram_count = 0
+    cart = request.session.get('cart', {})
+    #number_people_adult = request.form.get('number_people_adult')
+    #number_people_child = request.form.get('number_people_child')
+    # ttl_people = number_people_adult + number_people_child
 
     gift_event_1 = settings.GIFT_THRESHOLD_1
     gift_event_2 = settings.GIFT_THRESHOLD_2
 
+    for item_id, number_people_adult in cart.items():
+        tourprogram = get_object_or_404(Tourprogram, pk=item_id)
+        total_adult += number_people_adult * tourprogram.priceadult
+        #total_child += number_people_child * tourprogram.pricechild
+        
+        tourprogram_count += number_people_adult
+        cart_items.append({
+            'item_id': item_id,
+            'number_people_adult': number_people_adult,
+            #'number_people_child': number_people_child,
+            'tourprogram': tourprogram,
+        })
     
-    grand_total = total
+    grand_total = total_adult
+    # + total_child
     
     context = {
         'cart_items': cart_items,
-        'total': total,
+        'total_adult': total_adult,
+        # 'total_child': total_child,
         'tourprogram_count': tourprogram_count,
         'gift_event_1': gift_event_1,
         'gift_event_2': gift_event_2,
--- cart/views.py
+++ cart/views.py
@@ -10,15 +10,16 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    number_people = int(request.POST.get('number_people'))
+    number_people_adult = int(request.POST.get('number_people_adult'))
+    #number_people_child = int(request.POST.get('number_people_child'))
+    #ttl_people = number_people_adult + number_people_child
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
-        cart[item_id] += number_people
+        cart[item_id] += number_people_adult #ttl_people
     else:
-        cart[item_id] = number_people
+        cart[item_id] = number_people_adult #ttl_people
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
