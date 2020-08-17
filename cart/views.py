7e762a70ad8177ba4e6899e8a4516e02eedc1d95
ss00831 <songhei_831.lee@hotmail.com>
2020-08-17 12:53:33 +0000

    Added toasts messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from tourprograms.models import Tourprogram
 
 # Create your views here.
 
@@ -10,6 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the cart  """
 
+    tourprogram = get_object_or_404(Tourprogram, pk=item_id)
     number_people_adult = int(request.POST.get('number_people_adult'))
     #number_people_child = int(request.POST.get('number_people_child'))
     #ttl_people = number_people_adult + number_people_child
@@ -18,8 +22,10 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += number_people_adult
+        messages.success(request, f'Updated {tourprogram.name} people to {cart[item_id]}')
     else:
         cart[item_id] = number_people_adult
+        messages.success(request, f'Added {tourprogram.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -28,14 +34,17 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the number of people of the specified tour program to the specified amount"""
 
+    tourprogram = get_object_or_404(Tourprogram, pk=item_id)
     number_people_adult = int(request.POST.get('number_people_adult'))
     
     cart = request.session.get('cart', {})
 
     if number_people_adult > 0:
         cart[item_id] = number_people_adult
+        messages.success(request, f'Updated {tourprogram.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {tourprogram.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -45,15 +54,18 @@ def remove_from_cart(request, item_id):
     """Remove the item from the cart"""
 
     try:
+        tourprogram = get_object_or_404(Tourprogram, pk=item_id)
         size = None
         if 'tourprogram_size' in request.POST:
             size = request.POST['tourprogram_size']
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request, f'Removed {tourprogram.name} from your bag')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500) 
