b80fe52ffc4de87efaac9e02cc3c04691e607510
ss00831 <songhei_831.lee@hotmail.com>
2020-08-24 13:09:57 +0000

    Complete checkout and stripe (functional)

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,11 +11,13 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                       'order_total', 'grand_total',)
+                       'order_total', 'grand_total',
+                       'original_cart', 'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_cart',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'grand_total',)
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -80,17 +80,17 @@ form.addEventListener('submit', function(ev) {
                         //state: $.trim(form.county.value),
                     }
                 }
-            },
-            /*shipping: {
+            },/*
+            shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
                 address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
+                    //line1: $.trim(form.street_address1.value),
+                    //line2: $.trim(form.street_address2.value),
+                    //city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
+                    //postal_code: $.trim(form.postcode.value),
+                    //state: $.trim(form.county.value),
                 }
             },*/
         }).then(function(result) {
