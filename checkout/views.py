4db29189ab0af4c45f77beb856f90f7ff2324abc
ss00831 <songhei_831.lee@hotmail.com>
2020-08-24 06:36:27 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HJYpHH6B3368Fuk7AIH3mJgvjRdkttyBmkmLnb6OmlvhMsdBdXxEJTyZQYatsAUrKmdYVicEfO2AcQGDKcSgPHG00lAHPsURS',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
