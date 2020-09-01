a910ee42e1bc61a6329ca1cc25a214e60141a508
ss00831 <songhei_831.lee@hotmail.com>
2020-09-01 09:25:42 +0000

    Deployment 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lapland_in_sweden.wsgi:application
--- lapland_in_sweden/settings.py
+++ lapland_in_sweden/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'v4f35zzs85s_q_a4pr&&=5e_zz9ba05aqd)ub9q&45*x=am4-7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lapland-in-sweden.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +120,17 @@ WSGI_APPLICATION = 'lapland_in_sweden.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
